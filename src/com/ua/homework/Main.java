5f7446751e18b11c4485262b1fb13660efc5a0d1
Maryna Kilchitska <marina-kil@ukr.net>
2023-01-16 19:33:14 +0200

    Initial commit

---
+++ src/com/ua/homework/Main.java
@@ -0,0 +1,8 @@
+package com.ua.homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Я починаю курс Java в robot_dreams і вже вмію використовувати Git");
+    }
+}
